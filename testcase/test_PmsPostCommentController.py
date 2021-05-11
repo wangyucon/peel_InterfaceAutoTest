6a16c950707eb150dfe7174282495dd94a80a311
wangyu <wangyucon@163.com>
2021-05-11 16:40:49 +0800

    add 部分用例注释

--- testcase/test_PmsPostCommentController.py
+++ testcase/test_PmsPostCommentController.py
@@ -1,3 +1,6 @@
+"""
+评论模块测试用例
+"""
 import unittest
 import requests
 from peel_interface.common.read_excel import *
@@ -16,19 +19,19 @@ class TestCommentController(unittest.TestCase):
             cls.temp = yaml.load(f.read(), Loader=yaml.FullLoader)
             print(cls.temp['token'])
 
-
+    # 获取空评论列表
     def test_comment_null(self):
 
         case_data = get_test_data(self.data_list, 'test_comment_null')
-        if not case_data:  # 有可能为None
+        if not case_data:
             print("用例数据不存在")
         url = case_data.get('url')
-        expect_res = case_data.get('expect_res')  # 期望数据
+        expect_res = case_data.get('expect_res')
 
         response = requests.get(url=url)
         self.assertIn(expect_res, response.text)
 
-
+    # 获取非空评论列表
     def test_comment(self):
 
         case_data = get_test_data(self.data_list, 'test_comment')
@@ -40,7 +43,7 @@ class TestCommentController(unittest.TestCase):
         response = requests.get(url=url)
         self.assertIn(expect_res, response.text)
 
-
+    # 获取二级评论列表
     def test_sencond_comment(self):
 
         case_data = get_test_data(self.data_list, 'test_sencond_comment')
@@ -52,10 +55,12 @@ class TestCommentController(unittest.TestCase):
         response = requests.get(url=url)
         self.assertIn(expect_res, response.text)
 
+    # 插入评论
     # def test_insert_comment(self):
-    #
+    # 插入二级评论
     # def test_insert_sencond_comment(self):
 
+    # 删除评论
     def test_delete_comment(self):
 
         headers = {'authorization': self.temp['token']}
